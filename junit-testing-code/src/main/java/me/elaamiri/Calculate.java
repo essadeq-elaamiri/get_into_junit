7152b25e102ba5208bbdfa63dc656a32dca1fd4d
essadeq-elaamiri <essadeq0701@gmail.com>
2022-11-15 18:04:10 +0100

    eclipse project 1st test

---
+++ junit-testing-code/src/main/java/me/elaamiri/Calculate.java
@@ -0,0 +1,10 @@
+package me.elaamiri;
+
+public class Calculate {
+	
+	public int sum(int var1, int var2) {
+		System.out.println(String.format("Adding %d and %d", var1, var2));
+		return var1+var2;
+	}
+
+}
