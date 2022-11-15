467976dc42288e92fb9e93a4513c7421a9a45876
essadeq-elaamiri <essadeq0701@gmail.com>
2022-11-15 19:34:15 +0100

    annotations

--- junit-testing-code/src/test/java/me/elaamiri/CalculateTest.java
+++ junit-testing-code/src/test/java/me/elaamiri/CalculateTest.java
@@ -1,6 +1,18 @@
 package me.elaamiri;
 
- 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
 public class CalculateTest {
+	Calculate calculate = new Calculate();
 	
+	int sum = calculate.sum(5, 10);
+	int testSum = 15;
+	
+	@Test
+	public void testSum2Integers() {
+		System.out.println("@Test sum(): " + sum + " = " + testSum);
+		assertEquals(sum, testSum);
+	}
 }
