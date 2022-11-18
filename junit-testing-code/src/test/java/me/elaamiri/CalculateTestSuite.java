d850a2a8a3649c6a9c8af5dfdb3b77f16171a0b1
essadeq-elaamiri <essadeq0701@gmail.com>
2022-11-18 11:55:15 +0100

    example Suite

---
+++ junit-testing-code/src/test/java/me/elaamiri/CalculateTestSuite.java
@@ -0,0 +1,11 @@
+package me.elaamiri;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({CalculateTest.class, CalculateTest2.class, CalculateTest3.class})
+public class CalculateTestSuite {
+
+}
